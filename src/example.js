95bfdea76dd7c7098f32db0346ba462cdb3bf32e
杨君华 <yangjh@gegejia.com>
2017-06-20 14:53:03 +0800

    add drawRewardsByCanvas method

--- src/example.js
+++ src/example.js
@@ -1,79 +1,103 @@
-// import LotteryRoulette from './index';
-
-// const lotteryRoulette = new LotteryRoulette({
-//     img: document.querySelector('.prizes')
-// });
-// const btns = document.querySelectorAll('button');
-
-
-// let delta = 0;
-// [].forEach.call(btns, (btn, i) => {
-//     btn.addEventListener('click', () => {
-//         const deg = 360 - (i + 1 + i) * 30;
-//         // console.log(deg);
-//         lotteryRoulette.run(deg, 3000);
-//     });
-// });
-
 import './example.scss';
 import * as utils from './utils';
 
-const canvas = document.querySelector('canvas');
+/**
+ * @function drawRewardsByCanvas 通过canvas来绘制奖品
+ * @param {Object} options 配置对象
+ * @property {Object} options.canvasWidth 画布宽
+ * @property {Number} options.canvasHeight 画布高
+ * @property {Number} options.fontSize 字体大小
+ * @property {Number} options.fontFamily 字体类型
+ * @property {String} options.fillStyle 字体填充颜色
+ * @property {String} options.textAlign 文本水平对齐方式
+ * @property {String} options.textBaseline 文本垂直对齐方式
+ * @property {Array} options.bgFillStyles 背景填充颜色
+ * @property {Number} options.textDistance 文本距离外围圆弧的距离
+ * @property {Array} options.rewards 奖品
+ */
 
-// 必须先初始化大小，否则将会影响字体
-utils.initRetinaCanvasSize(canvas, 360, 360);
+function drawRewardsByCanvas(canvas, options = {}) {
+    options = Object.assign({
+        fontSize: 16,
+        fontFamily: 'Microsoft YaHei',
+        textBaseline: 'top',
+        textAlign: 'center'
+    }, options);
 
-const context = canvas.getContext('2d');
-context.font = `${utils.getRetinaValue(16)}px Microsoft YaHei`;
-context.textBaseline = 'top';
-
-const length = 6;
-const baseAngle = 360 / length;
-const radius = canvas.width / 2;
-let startAngle = 0;
-let endAngle = 0;
-
-for (let i = 0; i < length; i++) {
-    startAngle = i * baseAngle;
-    endAngle = (i + 1) * baseAngle;
-
-    context.save();
-
-    // 填充每块区域的背景色调 start ******************
-    context.beginPath();
-    context.arc(radius, radius, radius, utils.toRadian(startAngle), utils.toRadian(endAngle), false);
-    context.arc(radius, radius, utils.getRetinaValue(30), utils.toRadian(endAngle), utils.toRadian(startAngle), true);
-    context.closePath();
-
-    if (i % 2 === 0) {
-        context.fillStyle = '#ffdf3e';
-    } else {
-        context.fillStyle = '#fabe00';
+    if (!options.canvasHeight) {
+        options.canvasHeight = options.canvasWidth;
     }
-    context.fill();
-    // 画每块区域的背景色调 end ******************
+
+    // 必须先初始化大小，否则将会影响字体
+    utils.initRetinaCanvasSize(canvas, options.canvasWidth, options.canvasHeight);
+
+    const context = canvas.getContext('2d');
+    context.font = `${utils.getRetinaValue(options.fontSize)}px ${options.fontFamily}`;
+    context.textBaseline = options.textBaseline;
+    context.textAlign = options.textAlign;
+    context.fillStyle = options.fillStyle;
+
+    const length = options.rewards.length;
+    const baseAngle = 360 / length;
+    const radius = canvas.width / 2;
+    let startAngle = 0;
+    let endAngle = 0;
+
+    for (let i = 0; i < length; i++) {
+        startAngle = i * baseAngle;
+        endAngle = (i + 1) * baseAngle;
+
+        context.save();
+
+        // 填充每块区域的背景色调 start ******************
+        context.beginPath();
+        context.arc(radius, radius, radius, utils.toRadian(startAngle), utils.toRadian(endAngle), false);
+        context.arc(radius, radius, utils.getRetinaValue(30), utils.toRadian(endAngle), utils.toRadian(startAngle), true);
+        context.closePath();
+
+        const styleIndex = i % options.bgFillStyles.length;
+        context.fillStyle = options.bgFillStyles[styleIndex];
+        context.fill();
+        // 画每块区域的背景色调 end ******************
 
 
-    // 绘制奖品文字 start ******************
-    context.beginPath();
-    context.translate(radius, radius);
+        // 绘制奖品文字 start ******************
+        context.beginPath();
+        context.translate(radius, radius);
 
-    const centerAngle = startAngle + baseAngle / 2;
-    const translateX = Math.cos(utils.toRadian(centerAngle)) * Math.cos(utils.toRadian(baseAngle / 2)) * radius * 3/4;
-    const translateY = Math.sin(utils.toRadian(centerAngle)) * Math.cos(utils.toRadian(baseAngle / 2)) * radius * 3/4 ;
-    context.translate(translateX, translateY);
-    context.textBaseline = 'top';
+        const centerAngle = startAngle + baseAngle / 2;
+        const translateX = Math.cos(utils.toRadian(centerAngle)) * Math.cos(utils.toRadian(baseAngle / 2)) * radius * 3/4;
+        const translateY = Math.sin(utils.toRadian(centerAngle)) * Math.cos(utils.toRadian(baseAngle / 2)) * radius * 3/4 ;
+        context.translate(translateX, translateY);
+        context.rotate(utils.toRadian(centerAngle + 90));
 
-    context.rotate(utils.toRadian(centerAngle + 90));
-    context.textAlign = 'center';
-    context.fillStyle = 'green';
-    context.fillText(`${i+1}00元`, 0, 0);
+        const texts = options.rewards[i];
+        context.fillStyle = options.fillStyle;
+        for (let j = 0; j < texts.length; j++) {
+            context.fillText(texts[j], 0, utils.getRetinaValue(20) * j);
+        }
+        // 绘制奖品文字 end ******************
 
-    if (i === 0) {
-        context.fillText(`${i+1}00元哦`, 0, utils.getRetinaValue(20));
+        // 每次循环后恢复画布初始状态
+        context.restore();
     }
-    // 绘制奖品文字 end ******************
-
-    // 每次循环后恢复画布初始状态
-    context.restore();
 }
+
+drawRewardsByCanvas(document.querySelector('canvas'), {
+    canvasWidth: 360,
+    fontSize: 16,
+    bgFillStyles: ['#ffdf3e', '#fabe00'],
+    fillStyle: 'green',
+    rewards: [
+        ['100元', '100元+'],
+        ['200元'],
+        ['300元'],
+        ['400元'],
+        ['500元'],
+        ['600元'],
+        ['700元'],
+        ['800元'],
+        ['900元'],
+        ['1000元'],
+    ]
+});
