0a19364c9ff625d42744c7a11693ad8ec2bc32a3
杨君华 <yangjh@gegejia.com>
2017-06-30 18:03:39 +0800

    complete example

--- src/example.js
+++ src/example.js
@@ -1,105 +1,44 @@
 import './example.scss';
-import * as utils from './utils';
+import LotteryRoulette from './index';
 
-/**
- * @function drawRewardsByCanvas 通过canvas来绘制奖品
- * @param {Object} options 配置对象
- * @property {Object} options.canvasWidth 画布宽
- * @property {Number} options.canvasHeight 画布高
- * @property {Array} options.bgFillStyles 背景填充颜色
- * @property {Number} options.fontSize 字体大小
- * @property {Number} options.fontFamily 字体类型
- * @property {String} options.textAlign 文本水平对齐方式
- * @property {String} options.textBaseline 文本垂直对齐方式
- * @property {String} options.textFillStyle 字体填充颜色
- * @property {Number} options.textMarginTop 文本距离圆弧的距离
- * @property {Array} options.rewards 奖品
- */
-
-function drawRewardsByCanvas(canvas, options = {}) {
-    options = Object.assign({
-        fontSize: 16,
-        fontFamily: 'Microsoft YaHei',
-        textBaseline: 'top',
-        textAlign: 'center',
-        textMarginTop: 0
-    }, options);
-
-    if (!options.canvasHeight) {
-        options.canvasHeight = options.canvasWidth;
-    }
-
-    // 必须先初始化大小，否则将会影响字体
-    utils.initRetinaCanvasSize(canvas, options.canvasWidth, options.canvasHeight);
-
-    const context = canvas.getContext('2d');
-    context.font = `${utils.getRetinaValue(options.fontSize)}px ${options.fontFamily}`;
-    context.textBaseline = options.textBaseline;
-    context.textAlign = options.textAlign;
-    context.textMarginTop = options.textMarginTop;
-
-    const length = options.rewards.length;
-    const baseAngle = 360 / length;
-    const radius = canvas.width / 2;
-    let startAngle = 0;
-    let endAngle = 0;
-
-    for (let i = 0; i < length; i++) {
-        startAngle = i * baseAngle;
-        endAngle = (i + 1) * baseAngle;
-
-        context.save();
-
-        // 填充每块区域的背景色调 start ******************
-        context.beginPath();
-        context.arc(radius, radius, radius, utils.toRadian(startAngle), utils.toRadian(endAngle), false);
-        context.arc(radius, radius, utils.getRetinaValue(0), utils.toRadian(endAngle), utils.toRadian(startAngle), true);
-        context.closePath();
-
-        const styleIndex = i % options.bgFillStyles.length;
-        context.fillStyle = options.bgFillStyles[styleIndex];
-        context.fill();
-        // 画每块区域的背景色调 end ******************
-
-
-        // 绘制奖品文字 start ******************
-        context.beginPath();
-        context.translate(radius, radius);
-
-        const centerAngle = startAngle + baseAngle / 2;
-        const translateX = Math.cos(utils.toRadian(centerAngle)) * (radius - utils.getRetinaValue(options.textMarginTop));
-        const translateY = Math.sin(utils.toRadian(centerAngle)) * (radius - utils.getRetinaValue(options.textMarginTop));
-        context.translate(translateX, translateY);
-        context.rotate(utils.toRadian(centerAngle + 90));
-
-        const texts = options.rewards[i];
-        context.fillStyle = options.textFillStyle;
-        for (let j = 0; j < texts.length; j++) {
-            context.fillText(texts[j], 0, utils.getRetinaValue(20) * j);
-        }
-        // 绘制奖品文字 end ******************
-
-        // 每次循环后恢复画布初始状态
-        context.restore();
-    }
-}
-
-drawRewardsByCanvas(document.querySelector('canvas'), {
+const lotteryRoulette = new LotteryRoulette({
+    canvas: document.querySelector('canvas'),
     canvasWidth: 360,
     bgFillStyles: ['#ffdf3e', '#fabe00'],
     fontSize: 16,
     textFillStyle: 'green',
     textMarginTop: 50,
     rewards: [
-        ['100元', '100元+'],
-        ['200元'],
-        ['300元'],
-        ['400元'],
-        ['500元'],
-        ['600元'],
-        ['700元'],
-        ['800元'],
-        // ['900元'],
-        // ['1000元'],
+        ['奖品1', '奖品1+'],
+        ['奖品2'],
+        ['奖品3'],
+        ['奖品4'],
+        ['奖品5'],
+        ['奖品6'],
+        // ['奖品7'],
+        // ['奖品8'],
+        // ['奖品9'],
+        // ['奖品10'],
     ]
 });
+
+
+document.querySelector('.btn1').addEventListener('click', function(e) {
+    lotteryRoulette.run(1, 5000);
+});
+document.querySelector('.btn2').addEventListener('click', function(e) {
+    lotteryRoulette.run(2, 5000);
+});
+document.querySelector('.btn3').addEventListener('click', function(e) {
+    lotteryRoulette.run(3, 5000);
+});
+document.querySelector('.btn4').addEventListener('click', function(e) {
+    lotteryRoulette.run(4, 5000);
+});
+document.querySelector('.btn5').addEventListener('click', function(e) {
+    lotteryRoulette.run(5, 5000);
+});
+document.querySelector('.btn6').addEventListener('click', function(e) {
+    lotteryRoulette.run(6, 5000);
+});
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+import * as utils from './utils';
+
 /**
  * @class LotteryRoulette 抽奖轮盘
  * created by yangjunhua on 17/06/16
  */
 
 export default class LotteryRoulette {
+    /**
+     * @function 构造器
+     * @param {Object} options 配置对象
+     * @property {DOM} options.canvas 指定页面canvas元素
+     * @property {Array} options.bgFillStyles 背景填充颜色
+     * @property {Number} options.fontSize 字体大小
+     * @property {Number} options.fontFamily 字体类型
+     * @property {String} options.textAlign 文本水平对齐方式
+     * @property {String} options.textBaseline 文本垂直对齐方式
+     * @property {String} options.textFillStyle 字体填充颜色
+     * @property {Number} options.textMarginTop 文本距离圆弧的距离
+     * @property {Array} options.rewards 奖品
+     */
     constructor(options) {
         this.init(options);
     }
@@ -13,73 +28,42 @@ export default class LotteryRoulette {
      * @param {Object} options 配置对象
      */
     init(options) {
-        this.canvas = document.createElement('canvas');
+        this.canvas = options.canvas;
         this.context = this.canvas.getContext('2d');
-        this.img = {};
+
+        this.options = Object.assign({
+            fontSize: 16,
+            fontFamily: 'Microsoft YaHei',
+            textBaseline: 'top',
+            textAlign: 'center',
+            textMarginTop: 0
+        }, options || {});
+
+        if (!this.options.canvasHeight) {
+            this.options.canvasHeight = this.options.canvasWidth;
+        }
+        utils.initRetinaCanvasSize(this.canvas, this.options.canvasWidth, this.options.canvasHeight);
 
         this.rafId = null;
         this.lastMotionDegrees = 0;
         this.starting = false;
-        this.callback = options.callback || function() {};
 
-        const imgLoadedHandler = () => {
-            this.imgLoaded = true;
-
-            this.saveImgSize(options.img);
-            this.img.el = options.img;
-            this.canvas.width = this.img.width;
-            this.canvas.height = this.img.height;
-            this.drawImg(0);
-
-            options.img.parentNode.insertBefore(this.canvas, options.img);
-            options.img.parentNode.removeChild(options.img);
-        };
-
-        if (options.img.complete) {
-            imgLoadedHandler();
-        } else {
-            options.img.onload = imgLoadedHandler;
-        }
-    }
-
-    /**
-     * @method saveImgSize 保存图片大小
-     * @param {Element} img
-     */
-    saveImgSize(img) {
-        const styleObj = window.getComputedStyle(img);
-        this.img.width = parseInt(styleObj.getPropertyValue('width'), 10);
-        this.img.height = parseInt(styleObj.getPropertyValue('height'), 10);
-        this.img.naturalWidth = img.naturalWidth;
-        this.img.naturalHeight = img.naturalHeight;
-    }
-
-    /**
-     * @method drawImg 绘图
-     * @param {Number} degree 旋转角度
-     */
-    drawImg(degree) {
-        const radian = Math.PI / 180 * (degree + this.lastMotionDegrees);
-
-        this.context.clearRect(0, 0, this.img.width, this.img.height);
-        this.context.beginPath();
-        this.context.setTransform(1, 0, 0, 1, 0, 0);
-        this.context.transform(Math.cos(radian), Math.sin(radian), -Math.sin(radian), Math.cos(radian), this.img.width / 2, this.img.height / 2);
-        this.context.drawImage(this.img.el, 0, 0, this.img.naturalWidth, this.img.naturalHeight, -this.img.width / 2, -this.img.height / 2, this.img.width, this.img.height);
+        this.initMotionDegrees = 270 - (360 / this.options.rewards.length / 2);
+        this.drawCanvas(this.initMotionDegrees, this.drawRewardsByCanvas());
     }
 
     /**
      * @method run 开始转动轮盘
-     * @param {Number} radians 需要转动多少角度，如：720deg
+     * @param {Number} toIndex 转动到哪个奖品区域，从1开始
      * @param {Number} duration 轮盘转动的持续时间，单位：毫秒
      */
-    run(degrees, duration) {
-        if (!this.imgLoaded) return;
+    run(toIndex, duration) {
         if (this.starting) return;
         this.starting = true;
 
+        const degrees = this.initMotionDegrees + 360 - (toIndex - 1) * (360 / this.options.rewards.length);
         let detalDegrees = 0;
-        let totalDegrees = degrees - this.lastMotionDegrees + 360 * 4;
+        let totalDegrees = degrees - this.lastMotionDegrees + 360 * 2;
         const motionStart = Date.now();
 
         const rafCallback = () => {
@@ -89,20 +73,96 @@ export default class LotteryRoulette {
             if (t > 0) {
                 this.starting = false;
                 this.lastMotionDegrees = degrees;
-                this.callback(this.lastMotionDegrees, totalDegrees);
+                this.options.callback && this.options.callback(this.lastMotionDegrees, totalDegrees);
                 return;
             }
-            this.drawImg(detalDegrees);
+            this.drawCanvas(detalDegrees, this.drawRewardsByCanvas());
             this.rafId = window.requestAnimationFrame(rafCallback);
         };
         this.rafId = window.requestAnimationFrame(rafCallback);
     }
 
     /**
+     * @method drawCanvas 绘制画布
+     * @param {Number} degree 旋转角度
+     * @param {DOM} cacheCanvas 离屏canvas
+     */
+    drawCanvas(degree, cacheCanvas) {
+        const radian = utils.toRadian(degree + this.lastMotionDegrees);
+        const radius = this.canvas.width / 2;
+
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.beginPath();
+        this.context.setTransform(1, 0, 0, 1, 0, 0);
+        this.context.transform(Math.cos(radian), Math.sin(radian), -Math.sin(radian), Math.cos(radian), radius, radius);
+        this.context.drawImage(cacheCanvas, 0, 0, this.canvas.width, this.canvas.height, -radius, -radius, this.canvas.width, this.canvas.height);
+    }
+
+    /**
      * @method stop 停止轮盘转动
      */
     stop() {
-        if (!this.imgLoaded) return;
         window.cancelAnimationFrame(this.rafId);
+    }
+
+    drawRewardsByCanvas() {
+        const options = this.options || {};
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+
+        canvas.width = this.canvas.width;
+        canvas.height = this.canvas.height;
+
+        context.font = `${utils.getRetinaValue(options.fontSize)}px ${options.fontFamily}`;
+        context.textBaseline = options.textBaseline;
+        context.textAlign = options.textAlign;
+        context.textMarginTop = options.textMarginTop;
+
+        const length = options.rewards.length;
+        const baseAngle = 360 / length;
+        const radius = canvas.width / 2;
+        let startAngle = 0;
+        let endAngle = 0;
+
+        for (let i = 0; i < length; i++) {
+            startAngle = i * baseAngle;
+            endAngle = (i + 1) * baseAngle;
+
+            context.save();
+
+            // 填充每块区域的背景色调 start ******************
+            context.beginPath();
+            context.arc(radius, radius, radius, utils.toRadian(startAngle), utils.toRadian(endAngle), false);
+            context.arc(radius, radius, utils.getRetinaValue(0), utils.toRadian(endAngle), utils.toRadian(startAngle), true);
+            context.closePath();
+
+            const styleIndex = i % options.bgFillStyles.length;
+            context.fillStyle = options.bgFillStyles[styleIndex];
+            context.fill();
+            // 画每块区域的背景色调 end ******************
+
+
+            // 绘制奖品文字 start ******************
+            context.beginPath();
+            context.translate(radius, radius);
+
+            const centerAngle = startAngle + baseAngle / 2;
+            const translateX = Math.cos(utils.toRadian(centerAngle)) * (radius - utils.getRetinaValue(options.textMarginTop));
+            const translateY = Math.sin(utils.toRadian(centerAngle)) * (radius - utils.getRetinaValue(options.textMarginTop));
+            context.translate(translateX, translateY);
+            context.rotate(utils.toRadian(centerAngle + 90));
+
+            const texts = options.rewards[i];
+            context.fillStyle = options.textFillStyle;
+            for (let j = 0; j < texts.length; j++) {
+                context.fillText(texts[j], 0, utils.getRetinaValue(20) * j);
+            }
+            // 绘制奖品文字 end ******************
+
+            // 每次循环后恢复画布初始状态
+            context.restore();
+        }
+
+        return canvas;
     }
 }
